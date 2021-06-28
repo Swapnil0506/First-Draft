af9f21997a4209846238af0dcd6e5123ba39882e
Swapnil Jha <59622008+Swapnil0506@users.noreply.github.com>
2021-06-28 14:53:46 +0530

    Updated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn blog_app.wsgi --log file -
+web: gunicorn blog_app.wsgi --log-file -
--- blog_app/settings.py
+++ blog_app/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'blog_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
