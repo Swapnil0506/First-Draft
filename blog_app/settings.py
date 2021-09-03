b3965db2fb49ebcdc8f9edc881eacbf52a81ee26
Swapnil Jha <59622008+Swapnil0506@users.noreply.github.com>
2021-09-03 13:10:29 +0530

    Updates done

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -151,6 +151,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'thebodyworkks@gmail.com'
-EMAIL_HOST_PASSWORD = 'passwork11'
+EMAIL_HOST_PASSWORD = 'loletacjypzribwn'
 
 django_heroku.settings(locals())
