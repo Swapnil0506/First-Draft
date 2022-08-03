769948a85e4701cfcc05630921a7d97b706fe53b
Swapnil Jha <59622008+Swapnil0506@users.noreply.github.com>
2022-08-03 20:57:57 +0530

    Google SMTP Changes

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -151,6 +151,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'thebodyworkks@gmail.com'
-EMAIL_HOST_PASSWORD = 'loletacjypzribwn'
+EMAIL_HOST_PASSWORD = 'isgrlwkhykucxxma'
 
 django_heroku.settings(locals())
